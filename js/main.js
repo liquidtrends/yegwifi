3478656504314fe6dacc3adfb6a97b63937297dc
Ryan Janvier <ryan@liquidtrends.com>
2014-12-03 10:17:15 -0800

    Inital Commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$.getJSON('https://data.edmonton.ca/resource/vbxz-36ag.json', function(data) {
+   
+   var lat = data.location.latitude
+   var lng = data.location.longitude
+   // var name = data[1].location_name
+
+
+
+});
